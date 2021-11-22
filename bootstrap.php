d93f3fa8d7b5eae8a4e62360e99aff22e44ff8d3
enricocesca <47447292+enricocesca@users.noreply.github.com>
2021-11-22 09:34:01 +0100

    Setup del Progetto

---
+++ bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * bootstrap.php
+ */
+ 
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "vendor/autoload.php";
+
+$isDevMode = true;
+$proxyDir = null;
+$cache = null;
+$useSimpleAnnotationReader = false;
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
+
+$conn = array(
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__ . '/db.sqlite',
+);
+
+$entityManager = EntityManager::create($conn, $config);
+
+?>
